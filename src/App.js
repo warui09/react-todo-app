62560323bdf3de6b9b978837c2b52c69df6b8ed1
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-24 12:08:45 +0300

    rendered TodoContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import TodoContainer from "./components/TodoContainer";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <TodoContainer />
     </div>
   );
 }
