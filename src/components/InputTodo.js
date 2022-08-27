18900db5b7665a291ad0ce3fde0ce23b2051eb81
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-27 16:16:51 +0300

    added method to prevent form default behavior

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -4,10 +4,27 @@ class InputTodo extends Component {
   state = {
     title: ""
   };
+
+  onChange = (e) => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.title);
+  };
+
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Add todo..." />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     );
