4869f4b9f3fbd29fc42720606571b0c3719b9ce0
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-27 12:08:14 +0300

    defined the delTodo function

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -37,7 +37,13 @@ class TodoContainer extends React.Component {
   };
 
   delTodo = (id) => {
-    console.log("deleted", id);
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id
+        })
+      ]
+    })
   };
 
   render() {
