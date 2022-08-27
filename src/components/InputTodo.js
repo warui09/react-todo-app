79505146c8a98c8858fdef15710b4ccea166daf0
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-27 17:44:11 +0300

    refactored InputTodo to prevent adding empty string

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -13,8 +13,12 @@ class InputTodo extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({ title: "" });
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({ title: "" });
+    } else {
+      alert("Write item");
+    }
   };
 
   render() {
