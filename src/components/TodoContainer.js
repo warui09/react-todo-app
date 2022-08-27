a53b9303506d9932aeb73b6d66d0e3264ff50e3d
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-27 16:44:59 +0300

    refactored addTodoItem to allow adding todo items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -48,7 +49,14 @@ class TodoContainer extends React.Component {
   };
 
   addTodoItem = (title) => {
-    console.log(title);
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
   };
 
   render() {
