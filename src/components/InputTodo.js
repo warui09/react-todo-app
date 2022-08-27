7e222601923d58e670eb2b5a31eed2a05af4af09
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-27 18:45:02 +0300

    added classes to InputTodo for styling purposes

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -23,14 +23,15 @@ class InputTodo extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
+          className="input-text"
           placeholder="Add todo..."
           value={this.state.title}
           onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     );
   }
