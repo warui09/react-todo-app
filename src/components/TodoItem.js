be40e41382a3fa81cab8ac75b037b96b5fc5938c
warui09 <93465580+warui09@users.noreply.github.com>
2022-08-29 10:34:41 +0300

    used css modules to style TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styles from "./TodoItem.module.css";
 
 class TodoItem extends React.Component {
   render() {
     return (
-      <li key={this.props.todo.id}>
+      <li key={this.props.todo.id} className={styles.item}>
         <input
           type={"checkbox"}
+          className={styles.checkbox}
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
